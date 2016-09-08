91245ccfa4d09fa2ed9dbd32fd3da035bdb576ae
billsmith <billsmithaustin@yahoo.com>
2016-09-08 12:15:19 -0500

    Use a scheduled runnable rather than a sleep loop

--- src/main/java/com/indeed/example/CodeCacheDataDog.java
+++ src/main/java/com/indeed/example/CodeCacheDataDog.java
@@ -3,32 +3,43 @@ package com.indeed.example;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryUsage;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.List;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
 
 public class CodeCacheDataDog {
-    public static void main(String[] args) throws InterruptedException {
-	final String hostname = args[0];
-	final int port = Integer.parseInt(args[1]);
+    public static class Sampler implements Runnable {
+        final StatsDClient client;
+        final MemoryPoolMXBean codeCacheMXBean;
 
-        final StatsDClient client = new NonBlockingStatsDClient("example", hostname, port);
+        public Sampler(final String prefix, final String hostname, final int port) {
+            this.client = new NonBlockingStatsDClient(prefix, hostname, port);
+            final List<MemoryPoolMXBean> memoryPoolMXBeans = ManagementFactory.getMemoryPoolMXBeans();
+            for (final MemoryPoolMXBean memoryPoolMXBean : memoryPoolMXBeans) {
+                if ("Code Cache".equals(memoryPoolMXBean.getName())) {
+                    codeCacheMXBean = memoryPoolMXBean;
+                    return;
+                }
+            }
 
-	MemoryPoolMXBean codeCacheMXBean = null;
-	final List<MemoryPoolMXBean> memoryPoolMXBeans = ManagementFactory.getMemoryPoolMXBeans();
-	for (final MemoryPoolMXBean memoryPoolMXBean : memoryPoolMXBeans) {
-	    if ("Code Cache".equals(memoryPoolMXBean.getName())) {
-		codeCacheMXBean = memoryPoolMXBean;
-		break;
-	    }
-	}
+            // Shouldn't get here unless a newer Java release renames Code Cache
+            throw new RuntimeException("Did not find Code Cache memory pool.");
+        }
 
-	while (true) {
-	    final MemoryUsage usage = codeCacheMXBean.getUsage();
-	    client.recordGaugeValue("codeCache.used", (double)usage.getUsed());
-	    client.recordGaugeValue("codeCache.max", (double)usage.getMax());
-	    Thread.sleep(30000);
-	}
+        public void run() {
+            final MemoryUsage usage = codeCacheMXBean.getUsage();
+            client.recordGaugeValue("codeCache.used", (double)usage.getUsed());
+            client.recordGaugeValue("codeCache.max", (double)usage.getMax());
+        }            
     }
 
+    public static void main(String[] args) throws InterruptedException {
+        final String hostname = args[0];
+        final int port = Integer.parseInt(args[1]);
+
+        final Sampler sampler = new Sampler("example", hostname, port);
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(sampler, 0, 30, TimeUnit.SECONDS);        
+    }
 }
